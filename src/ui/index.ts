fca1ea029f607a9201fe69ba9898a9c87e568f5c
sandgraal <92229510+sandgraal@users.noreply.github.com>
2025-10-24 15:44:22 -0600

    Add reusable event log export helpers

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,11 @@
 export * from "./components";
 export { GLOSSARY_ENTRIES } from "./glossary";
 export { TOOLTIP_COPY } from "./tooltips";
+export {
+  serializeEventLogNdjson,
+  mapEventsToExportRecords,
+  eventLogRowToExportItem,
+  type EventLogExportEvent,
+  type EventLogExportItem,
+  type EventLogExportRecord,
+} from "./eventLogExport";
